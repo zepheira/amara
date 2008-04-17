8b6f406df80bb54a153c89ba35d5e6bed524d3ab
Jeremy Kloth <jeremy.kloth@4suite.org>
2008-04-17 08:35:00 -0600

    Added domlette Python wrapper module

---
+++ amara/domlette.py
@@ -0,0 +1,7 @@
+#########################################################################
+# amara/domlette.py
+"""
+A very fast DOM-like library tailored for use in XPath/XSLT.
+"""
+
+from amara._domlette import *
