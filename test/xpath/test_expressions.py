b5a7684434a976f8b32e69e2d328f8f3ee42f274
Jeremy Kloth <jeremy.kloth@4suite.org>
2008-04-22 17:54:43 -0600

    Finished porting XPath expression tests.

---
+++ test/xpath/test_expressions.py
@@ -0,0 +1,233 @@
+import sys
+import unittest
+from xml.dom import Node
+from amara.lib import inputsource, testsupport
+from amara import domlette
+from amara.xpath import context, datatypes
+from amara.xpath.expressions import expression
+from amara.xpath.expressions.basics import string_literal, number_literal
+
+########################################################################
+# Literal expressions for testing expression results
+
+# Create an independent "literal" base class as to not confuse the isinstance()
+# checks throughout the compilation phase. The XPath parser cannot emit
+# boolean or node-set literals!
+class test_literal(expression):
+    def __init__(self, literal):
+        self._literal = self.return_value(literal)
+
+    def compile_as_boolean(self, compiler):
+        value = self._literal and datatypes.TRUE or datatypes.FALSE
+        compiler.emit('LOAD_CONST', value)
+        return
+
+    def compile_as_number(self, compiler):
+        try:
+            value = datatypes.number(self._literal)
+        except ValueError:
+            value = datatypes.NOT_A_NUMBER
+        compiler.emit('LOAD_CONST', value)
+        return
+
+    def compile_as_string(self, compiler):
+        try:
+            value = datatypes.string(self._literal)
+        except ValueError:
+            value = datatypes.EMPTY_STRING
+        compiler.emit('LOAD_CONST', value)
+        return
+
+    def pprint(self, indent='', stream=None):
+        print >> stream, indent + repr(self)
+
+
+class boolean_literal(test_literal):
+    return_value = datatypes.boolean
+
+    compile = test_literal.compile_as_boolean
+
+    def __str__(self):
+        return self._literal and 'true()' or 'false()'
+TRUE = boolean_literal(True)
+FALSE = boolean_literal(False)
+
+
+class nodeset_literal(test_literal):
+    return_value = datatypes.nodeset
+
+    def compile_iterable(self, compiler):
+        compiler.emit(# discard the context node
+                      'POP_TOP',
+                      # return the iterator directly
+                      'LOAD_CONST', iter(self._literal))
+
+    def compile_as_nodeset(self, compiler):
+        compiler.emit('LOAD_CONST', datatypes.nodeset,
+                      'LOAD_CONST', tuple(self._literal),
+                      'CALL_FUNCTION', 1,
+                      )
+    compile = compile_as_nodeset
+
+    def __str__(self):
+        def nodestr(node):
+            if isinstance(node, domlette.Document):
+                return '#document'
+            if isinstance(node, (domlette.Text, domlette.Comment)):
+                data = node.nodeValue
+                if len(data) > 20:
+                    data = data[:15] + '...' + data[-3:]
+                return '(%s: %s)' % (node.nodeName, data)
+            if isinstance(node, domlette.Element):
+                return '<%s>' % node.tagName
+            if isinstance(node, domlette.Attr):
+                return '@%s' % node.name
+        nodes = ', '.join(map(nodestr, self._literal))
+        return '<node-set: {%s}>' % (nodes,)
+EMPTY_NODESET = nodeset_literal([])
+
+
+# Create IEEE 754 special numbers only possible from computations
+class number_constant(number_literal):
+    def __init__(self, name, literal):
+        self._name = name
+        number_literal.__init__(self, literal)
+    def __str__(self):
+        return self._name
+NOT_A_NUMBER = number_constant('NaN', datatypes.NOT_A_NUMBER)
+POSITIVE_INFINITY = number_constant('Infinity', datatypes.POSITIVE_INFINITY)
+NEGATIVE_INFINITY = number_constant('-Infinity', datatypes.NEGATIVE_INFINITY)
+
+
+# Node constants
+src = inputsource("""<?xml version='1.0' encoding='ISO-8859-1'?>
+<!DOCTYPE ROOT [
+  <!ELEMENT CHILD2 (#PCDATA|GCHILD)*>
+  <!ATTLIST CHILD2 attr1 CDATA #IMPLIED
+                   CODE ID #REQUIRED>
+]>
+<?xml-stylesheet "Data" ?>
+<ROOT>
+  <!-- Test Comment -->
+  <CHILD1 attr1="val1" attr31="31">
+    <GCHILD name="GCHILD11"/>
+    <GCHILD name="GCHILD12"/>
+    Text1
+  </CHILD1>
+  <CHILD2 attr1="val2" CODE="1">
+    <GCHILD name="GCHILD21"/>
+    <GCHILD name="GCHILD22"/>
+  </CHILD2>
+  <foo:CHILD3 xmlns:foo="http://foo.com" foo:name="mike"/>
+  <lang xml:lang="en">
+    <foo xml:lang=""/>
+    <foo/>
+    <f\xf6\xf8/>
+  </lang>
+</ROOT>
+<?no-data ?>
+""", 'urn:domlette-test-tree')
+DOC = domlette.NonvalParse(src)
+
+def children(node, type=Node.ELEMENT_NODE):
+    return [ child for child in node if child.nodeType == type ]
+
+# `#document` nodes
+PI, PI2 = children(DOC, Node.PROCESSING_INSTRUCTION_NODE)
+ROOT = DOC.documentElement
+# `ROOT` nodes
+COMMENT = children(ROOT, Node.COMMENT_NODE)[0]
+CHILDREN = CHILD1, CHILD2, CHILD3, LANG = children(ROOT)
+# `CHILD1` nodes
+ATTR1 = CHILD1.getAttributeNodeNS(None, 'attr1')
+ATTR31 = CHILD1.getAttributeNodeNS(None, 'attr31')
+GCHILDREN1 = GCHILD11, GCHILD12 = children(CHILD1)
+TEXT1 = CHILD1.lastChild
+# `CHILD2` nodes
+ATTR2 = CHILD2.getAttributeNodeNS(None, 'attr1')
+IDATTR2 = CHILD2.getAttributeNodeNS(None, 'CODE')
+GCHILDREN2 = GCHILD21, GCHILD22 = children(CHILD2)
+# `CHILD3` nodes
+ATTR3 = CHILD3.getAttributeNodeNS('http://foo.com', 'name')
+# `lang` nodes
+LCHILDREN = LCHILD1, LCHILD2, NONASCIIQNAME = children(LANG)
+
+
+########################################################################
+# unittest support
+
+class test_expr(unittest.TestCase, object):
+    # The name of module where the class to be tested is defined
+    module_name = None
+    # The name of the class to be tested
+    class_name = None
+    # The typed evaluate method (aka, evaluate_as_XXX())
+    evaluate_method = None
+    # The return type of the expression class' evaluate method
+    return_type = None
+    # list of test cases to add; item is a 2-item tuple of (args, expected)
+    test_cases = ()
+
+    class __metaclass__(type):
+        def __init__(cls, name, bases, namespace):
+            if cls.module_name and cls.class_name:
+                module = __import__(cls.module_name, {}, {}, [cls.class_name])
+                cls.new_expression = getattr(module, cls.class_name)
+
+            digits = len(str(len(cls.test_cases)))
+            for count, test in enumerate(cls.test_cases):
+                test_method = cls.new_test_method(*test)
+                method_name = 'test_%s_%0*d' % (cls.class_name, digits, count) 
+                setattr(cls, method_name, test_method)
+
+        def new_test_method(cls, args, expected, ctx=None):
+            if cls.return_type and not isinstance(expected, cls.return_type):
+                expected = cls.return_type(expected)
+            def test_method(self):
+                expr = self.new_expression(*args)
+                evaluate_methods = [expr.evaluate]
+                if self.evaluate_method:
+                    evaluate_methods.append(getattr(expr, self.evaluate_method))
+                for evaluate in evaluate_methods:
+                    result = evaluate(ctx)
+                    msg = "expected '%s', not '%s'" % (type(expected),
+                                                       type(result))
+                    self.assertTrue(isinstance(result, type(expected)), msg)
+                    msg = '%s == %s' % (expected, result)
+                    self.assertEquals(expected, result, msg)
+
+            argstr = ', '.join(arg.encode('unicode_escape') 
+                               if isinstance(arg, unicode)
+                               else str(arg)
+                               for arg in args)
+            test_method.__doc__ = '%s(%s)' % (cls.class_name, argstr)
+            return test_method
+
+
+    def new_context(self, node=DOC):
+        return context(node, 1, 1)
+
+
+    def assertEquals(self, first, second, msg=None):
+        # convert nan's to strings to prevent IEEE 754-style compares
+        if isinstance(first, float) and isinstance(second, float):
+            if datatypes.number(first).isnan():
+                first = 'NaN'
+            if datatypes.number(second).isnan():
+                second = 'NaN'
+        # convert nodesets to lists to prevent XPath-style nodeset compares
+        elif isinstance(first, list) and isinstance(second, list):
+            first, second = list(first), list(second)
+        return unittest.TestCase.assertEquals(self, first, second, msg)
+    assertEqual = assertEquals
+
+
+if __name__ == '__main__':
+    testsupport.test_main(
+        # list of module names to test
+        'test_basic_expr',      # basic expressions
+        'test_function_calls',  # basic expressions
+        'test_nodeset_expr',    # basic expressions
+        'test_boolean_expr',    # number expressions
+        'test_number_expr',     # number expressions
+        )
---
+++ test/xpath/test_function_calls.py
@@ -0,0 +1,376 @@
+#!/usr/bin/env python
+from amara import domlette
+from amara.xpath import context, datatypes
+from amara.xpath.expressions.basics import string_literal, number_literal
+
+from test_expressions import (
+    test_expr,
+    # boolean literals
+    TRUE, FALSE,
+    # number literals (for special values)
+    NOT_A_NUMBER, POSITIVE_INFINITY, NEGATIVE_INFINITY,
+    # nodeset literals
+    nodeset_literal, ROOT, CHILD1, CHILD2, CHILD3, LCHILD1, LCHILD2
+    )
+
+# contexts for nodeset {CHILD1, CHILD2, CHILD3}
+CONTEXT1 = context(CHILD1, 1, 3)
+CONTEXT2 = context(CHILD2, 2, 3)
+# contexts for nodeset {LCHILD1, LCHILD2, NONASCIIQNAME}
+CONTEXTLANG1 = context(LCHILD1, 1, 3)
+CONTEXTLANG2 = context(LCHILD2, 2, 3)
+
+DOC = domlette.Document()
+EGG1 = DOC.appendChild(domlette.Element(None, 'egg0'))
+EGG1.appendChild(domlette.Text('0'))
+EGG2 = DOC.appendChild(domlette.Element(None, 'egg1'))
+EGG2.appendChild(domlette.Text('1'))
+EGG3 = DOC.appendChild(domlette.Element(None, 'egg0'))
+EGG3.appendChild(domlette.Text('0'))
+EGG4 = DOC.appendChild(domlette.Element(None, 'egg1'))
+EGG4.appendChild(domlette.Text('1'))
+EGG5 = DOC.appendChild(domlette.Element(None, 'egg0'))
+EGG5.appendChild(domlette.Text('0'))
+
+
+class test_function_call(test_expr):
+    module_name = 'amara.xpath.expressions.functioncalls'
+    class_name = 'function_call'
+
+
+class test_last_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['last', ()], datatypes.number(3), CONTEXT1),
+        (['last', ()], datatypes.number(3), CONTEXT2),
+        ]
+
+
+class test_position_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['position', ()], datatypes.number(1), CONTEXT1),
+        (['position', ()], datatypes.number(2), CONTEXT2),
+        ]
+
+
+class test_count_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['count', (nodeset_literal([ROOT, CHILD1]),)], datatypes.number(2), CONTEXT1)
+        ]
+
+
+class test_id_function(test_function_call):
+    evaluate_method = 'evaluate_as_nodeset'
+    return_type = datatypes.nodeset
+    test_cases = [
+        (['id', [number_literal('1')]], datatypes.nodeset([CHILD2]), CONTEXT1),
+        (['id', [string_literal('"1 1"')]], datatypes.nodeset([CHILD2]), CONTEXT1),
+        (['id', [string_literal('"0"')]], datatypes.nodeset(), CONTEXT1),
+        (['id', [string_literal('"0 1"')]], datatypes.nodeset([CHILD2]), CONTEXT1),
+        (['id', [string_literal('"0 1 1"')]], datatypes.nodeset([CHILD2]), CONTEXT1),
+        (['id', [string_literal('"0 0 1 1"')]], datatypes.nodeset([CHILD2]), CONTEXT1),
+        (['id', [nodeset_literal([EGG1])]], datatypes.nodeset(), CONTEXT1),
+        (['id', [nodeset_literal([EGG1, EGG2])]], datatypes.nodeset([CHILD2]), CONTEXT1),
+        (['id', [nodeset_literal([EGG1, EGG2, EGG3])]], datatypes.nodeset([CHILD2]), CONTEXT1),
+        (['id', [nodeset_literal([EGG1, EGG2, EGG3, EGG4])]], datatypes.nodeset([CHILD2]), CONTEXT1),
+        ]
+
+
+class test_local_name_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['local-name', [nodeset_literal([])]], datatypes.string(), CONTEXT1),
+        (['local-name', [nodeset_literal([CHILD3])]], datatypes.string('CHILD3'), CONTEXT1),
+        ]
+
+
+class test_namespace_uri_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['namespace-uri', [nodeset_literal([])]],
+         datatypes.string(), 
+         CONTEXT1),
+        (['namespace-uri', [nodeset_literal([CHILD3])]], 
+         datatypes.string('http://foo.com'), 
+         CONTEXT1),
+        ]
+
+
+class test_name_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['name', [nodeset_literal([CHILD3])]],
+         datatypes.string('foo:CHILD3'), 
+         CONTEXT1),
+        ]
+
+
+class test_string_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['string', [nodeset_literal([CHILD1])]],
+         datatypes.string('\n    \n    \n    Text1\n  '), 
+         CONTEXT1),
+        ]
+
+
+class test_concat_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['concat', [nodeset_literal([CHILD1]), string_literal('"3.14"'), string_literal('"Hi"')]],
+         datatypes.string('\n    \n    \n    Text1\n  3.14Hi'), 
+         CONTEXT1),
+        ]
+
+
+class test_startswith_function(test_function_call):
+    evaluate_method = 'evaluate_as_boolean'
+    return_type = datatypes.boolean
+    test_cases = [
+        (['starts-with', [nodeset_literal([CHILD1]), string_literal('"3.14"')]], 
+         datatypes.FALSE, CONTEXT1),
+        (['starts-with', [nodeset_literal([CHILD1]), nodeset_literal([CHILD1])]], 
+         datatypes.TRUE, CONTEXT1),
+        (['starts-with', [nodeset_literal([CHILD1]), string_literal('""')]], 
+         datatypes.TRUE, CONTEXT1),
+        ]
+
+
+class test_contains_function(test_function_call):
+    evaluate_method = 'evaluate_as_boolean'
+    return_type = datatypes.boolean
+    test_cases = [
+        (['contains', [nodeset_literal([CHILD1]), string_literal('"3.14"')]], 
+         datatypes.TRUE, CONTEXT1),
+        (['contains', [nodeset_literal([CHILD1]), nodeset_literal([CHILD1])]], 
+         datatypes.TRUE, CONTEXT1),
+        (['contains', [nodeset_literal([CHILD1]), string_literal('""')]], 
+         datatypes.TRUE, CONTEXT1),
+        ]
+
+
+class test_substring_before_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['substring-before', [string_literal('"3.14Hi"'), string_literal('"Hi"')]],
+         datatypes.string('3.14'), CONTEXT1),
+        (['substring-before', [string_literal('"3.14Hi"'), string_literal('""')]],
+         datatypes.string(), CONTEXT1),
+        ]
+
+
+class test_substring_after_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['substring-after', [string_literal('"3.14Hi"'), string_literal('"3.14"')]],
+         datatypes.string('Hi'), CONTEXT1),
+        (['substring-after', [string_literal('"3.14Hi"'), string_literal('""')]],
+         datatypes.string(), CONTEXT1),
+        ]
+
+
+class test_substring_after_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['substring', [string_literal('"3.14Hi"'), string_literal('"3.14"')]],
+         datatypes.string('14Hi'), CONTEXT1),
+        (['substring', [string_literal('"3.14Hi"'), string_literal('"3.14"'), number_literal('1')]], 
+         datatypes.string('1'), CONTEXT1),
+        (['substring', [string_literal('"12345"'), number_literal('2'), number_literal('3')]], 
+         datatypes.string('234'), CONTEXT1),
+        (['substring', [string_literal('"12345"'), number_literal('2')]], 
+         datatypes.string('2345'), CONTEXT1),
+        (['substring', [string_literal('"12345"'), number_literal('1.5'), number_literal('2.6')]], 
+         datatypes.string('234'), CONTEXT1),
+        (['substring', [string_literal('"12345"'), number_literal('0'), number_literal('3')]], 
+         datatypes.string('12'), CONTEXT1),
+        (['substring', [string_literal('"12345"'), NOT_A_NUMBER, number_literal('3')]], 
+         datatypes.string(''), CONTEXT1),
+        (['substring', [string_literal('"12345"'), number_literal('1'), NOT_A_NUMBER]], 
+         datatypes.string(''), CONTEXT1),
+        (['substring', [string_literal('"12345"'), number_literal('-42'), POSITIVE_INFINITY]], 
+         datatypes.string('12345'), CONTEXT1),
+        (['substring', [string_literal('"12345"'), NEGATIVE_INFINITY, POSITIVE_INFINITY]], 
+         datatypes.string(''), CONTEXT1),
+        ]
+
+
+class test_string_length_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['string-length', [string_literal('"3.14Hi"')]],
+        datatypes.number(6), CONTEXT1),
+        ]
+
+
+class test_normalize_space_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        (['normalize-space', [string_literal('"Ht    	 There	   Mike"')]],
+         datatypes.string(u'Ht There Mike'), CONTEXT1),
+        ]
+
+
+class test_translate_function(test_function_call):
+    evaluate_method = 'evaluate_as_string'
+    return_type = datatypes.string
+    test_cases = [
+        # a,b,c,d,e,f become A,B,C,D,E,F
+        (['translate', [string_literal('"Ht    	 There	   Mike"'), string_literal('abcdef'), string_literal('ABCDEF')]],
+         datatypes.string('Ht    \t ThErE\t   MikE'), CONTEXT1),
+        # e becomes a
+        (['translate', [string_literal('"hello world"'), string_literal('e'), string_literal('a')]],
+         datatypes.string('hallo world'), CONTEXT1),
+        # e becomes a; extra chars in To string are ignored
+        (['translate', [string_literal('"hello world"'), string_literal('e'), string_literal('abc')]],
+         datatypes.string('hallo world'), CONTEXT1),
+        # e becomes a; l is deleted
+        (['translate', [string_literal('"hello world"'), string_literal('el'), string_literal('a')]],
+         datatypes.string('hao word'), CONTEXT1),
+        # a,b,c,d,e,f become A,B,C,D,E,F;
+        # the first occurance of a char in the 2nd arg is the replacement
+        (['translate', [string_literal('"hello world"'), string_literal('abcdde'), string_literal('ABCDEF')]],
+         datatypes.string('hFllo worlD'), CONTEXT1),
+        # if there is a char in the 2nd arg with no char at the same pos
+        # in the 3rd arg, that char is removed from the 1st arg
+        (['translate', [string_literal('"hello world"'), string_literal('abcdefgh'), string_literal('')]],
+         datatypes.string('llo worl'), CONTEXT1),
+        ]
+
+
+class test_boolean_function(test_function_call):
+    evaluate_method = 'evaluate_as_boolean'
+    return_type = datatypes.boolean
+    test_cases = [
+        (['boolean', [string_literal('"3.14Hi"')]],
+         datatypes.TRUE, CONTEXT1),
+        ]
+
+
+class test_not_function(test_function_call):
+    evaluate_method = 'evaluate_as_boolean'
+    return_type = datatypes.boolean
+    test_cases = [
+        (['not', [string_literal('"3.14Hi"')]],
+         datatypes.FALSE, CONTEXT1),
+        ]
+
+
+class test_true_function(test_function_call):
+    evaluate_method = 'evaluate_as_boolean'
+    return_type = datatypes.boolean
+    test_cases = [
+        (['true', []], datatypes.TRUE, CONTEXT1),
+        ]
+
+
+class test_false_function(test_function_call):
+    evaluate_method = 'evaluate_as_boolean'
+    return_type = datatypes.boolean
+    test_cases = [
+        (['false', []], datatypes.FALSE, CONTEXT1),
+        ]
+
+
+class test_boolean_function(test_function_call):
+    evaluate_method = 'evaluate_as_boolean'
+    return_type = datatypes.boolean
+    test_cases = [
+        (['lang', [string_literal('en')]], datatypes.FALSE, CONTEXTLANG1),
+        (['lang', [string_literal('en')]], datatypes.TRUE, CONTEXTLANG2),
+        (['lang', [string_literal('')]], datatypes.TRUE, CONTEXTLANG1),
+        (['lang', [string_literal('')]], datatypes.FALSE, CONTEXTLANG2),
+        (['lang', [string_literal('foo')]], datatypes.FALSE, CONTEXTLANG1),
+        (['lang', [string_literal('foo')]], datatypes.FALSE, CONTEXTLANG2),
+        ]
+
+
+class test_number_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['number', []], datatypes.NOT_A_NUMBER, CONTEXT1),
+        ]
+
+
+class test_sum_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['sum', [nodeset_literal([EGG1, EGG2, EGG3, EGG4])]],
+         datatypes.number(2), CONTEXT1),
+        ]
+
+
+class test_floor_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['floor', [string_literal('"3.14"')]], 
+         datatypes.number(3), CONTEXT1),
+        (['floor', [NOT_A_NUMBER]],
+         datatypes.NOT_A_NUMBER, CONTEXT1),
+        (['floor', [POSITIVE_INFINITY]],
+         datatypes.POSITIVE_INFINITY, CONTEXT1),
+        (['floor', [NEGATIVE_INFINITY]],
+         datatypes.NEGATIVE_INFINITY, CONTEXT1),
+        (['floor', [number_literal('0.5')]],
+         datatypes.number(0), CONTEXT1),
+        (['floor', [number_literal('-0.5')]],
+         datatypes.number(-1), CONTEXT1),
+        ]
+
+
+class test_ceiling_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['ceiling', [string_literal('"3.14"')]],
+         datatypes.number(4), CONTEXT1),
+        (['ceiling', [NOT_A_NUMBER]],
+         datatypes.NOT_A_NUMBER, CONTEXT1),
+        (['ceiling', [POSITIVE_INFINITY]],
+         datatypes.POSITIVE_INFINITY, CONTEXT1),
+        (['ceiling', [number_literal('0.5')]],
+         datatypes.number(1), CONTEXT1),
+        (['ceiling', [number_literal('-0.5')]],
+         datatypes.number(-0.0), CONTEXT1),
+        ]
+
+
+class test_round_function(test_function_call):
+    evaluate_method = 'evaluate_as_number'
+    return_type = datatypes.number
+    test_cases = [
+        (['round', [string_literal('"3.14"')]],
+         datatypes.number(3), CONTEXT1),
+        (['round', [number_literal('-4.5')]],
+         datatypes.number(-4), CONTEXT1),
+        (['round', [NOT_A_NUMBER]],
+         datatypes.NOT_A_NUMBER, CONTEXT1),
+        (['round', [POSITIVE_INFINITY]],
+         datatypes.POSITIVE_INFINITY, CONTEXT1),
+        (['round', [NEGATIVE_INFINITY]],
+         datatypes.NEGATIVE_INFINITY, CONTEXT1),
+        (['round', [string_literal('"12345"')]],
+         datatypes.number(12345), CONTEXT1),
+        ]
+
+    
+if __name__ == '__main__':
+    testsupport.test_main()
