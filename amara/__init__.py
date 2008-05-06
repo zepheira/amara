ea78cd4d89fd47f8ef25eeedf52f2ff6f2d8c9c3
Jeremy Kloth <jeremy.kloth@4suite.org>
2008-05-06 15:18:09 -0600

    Changed variable keywords into an explicit attribute mapping.

--- amara/__init__.py
+++ amara/__init__.py
@@ -5,13 +5,13 @@ XML_NAMESPACE = u"http://www.w3.org/XML/1998/namespace"
 XMLNS_NAMESPACE = u"http://www.w3.org/2000/xmlns/"
 
 class Error(Exception):
-    def __init__(self, code, message, **kwds):
+    def __init__(self, code, message, attrs):
         Exception.__init__(self, code, message)
         self.code = code
         self.message = message
         # map keywords into attributes
-        for attr in kwds:
-            setattr(self, attr, kwds[attr])
+        for attr in attrs:
+            setattr(self, attr, attrs[attr])
 
     def __str__(self):
         return self.message
