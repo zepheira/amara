03d27e27c541ef4487f0ae6c271ebd4f4cc966f9
Uche Ogbuji <uche@ogbuji.net>
2008-07-01 11:53:42 -0600

    Add in string helper amara.lib.string

---
+++ amara/lib/string.py
@@ -0,0 +1,3 @@
+#IsNCName', 'IsName', 'IsNmtoken', 'IsQName', 'IsXml', 'IsXmlSpace', 'SplitQName', 'XmlStrLStrip', 'XmlStrRStrip', 'XmlStrStrip'
+from amara._xmlstring import *
+from string import *
