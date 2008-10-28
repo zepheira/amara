a7314b01bd07b77187193553869f06df1bfe3d27
Uche Ogbuji <uche@ogbuji.net>
2008-10-28 14:13:13 -0600

    Complete RDFa scraper

--- demo/rdfascrape.py
+++ demo/rdfascrape.py
@@ -14,39 +14,53 @@ For other examples see: http://esw.w3.org/topic/RDFa/Examples
 
 import sys
 import amara
+#from amara.bindery import html
+from amara import bindery
 from amara.writers.struct import *
 from amara.namespaces import *
+from amara.lib.xmlstring import *
+from amara.lib.iri import DEFAULT_RESOLVER
 
-doc = amara.parse(sys.argv[1])
-statement_elements = doc.xml_select(u'//@property|//@resource')
-print statement_elements
+DOCURI = sys.argv[1]
+doc = bindery.parse(DOCURI)
+
+try:
+    DOCURI = BASEURI = doc.html.head.base.href
+except:
+    BASEURI = None
+
+def absolutize(uriref):
+    try:
+        return DEFAULT_RESOLVER.normalize(uriref, DOCURI)
+    except:
+        return uriref
+
+def expand(data, context=None):
+    if context:
+        nss = context.xml_namespaces.copy()
+        prefix, qname = splitqname(unicode(data))
+        if prefix and prefix in nss:
+            return nss[prefix] + qname
+    return unicode(data)
+
+def handle_statement(elem):
+    subject = elem.xml_select(u'ancestor::*/@about')
+    subject = absolutize(subject[0].xml_value) if subject else DOCURI
+    if elem.xml_select(u'@property') and elem.xml_select(u'@content'):
+        return ( subject , expand(elem.property, elem), elem.content )
+    elif elem.xml_select(u'@property'):
+        return ( subject, expand(elem.property, elem), expand(elem) )
+    elif elem.xml_select(u'@rel') and elem.xml_select(u'@resource'):
+        return ( subject, expand(elem.rel, elem), elem.resource )
+    elif elem.xml_select(u'@rel') and elem.xml_select(u'@href'):
+        return ( subject, expand(elem.rel, elem), elem.href )
+    else:
+        return ()
+
+statement_elems = doc.xml_select(u'//*[@property|@resource|@rel]')
+triples = ( handle_statement(elem) for elem in statement_elems )
+
+for triple in triples:
+    print triple
 
 sys.exit(0)
-
-w = structwriter(indent=u"yes").feed(
-ROOT(
-  E((RDF_NAMESPACE, u'rdf:RDF'),
-    NS(u'skos', SKOS_NAMESPACE),
-    NS(u'z', ZTHES_NAMESPACE),
-    (
-      E(
-        (SKOS_NAMESPACE, u'skos:Concept'),
-        {(RDF_NAMESPACE, u'rdf:ID'): term.xml_select(u'string(termId)')},
-        E((SKOS_NAMESPACE, u'skos:prefLabel'), term.xml_select(u'string(termName)')),
-        (E((SKOS_NAMESPACE, u'skos:note'),
-          E((SKOS_NAMESPACE, u'skos:Note'),
-            E((RDF_NAMESPACE, u'rdf:label'), note.xml_select(u'string(@label)')),
-            E((RDF_NAMESPACE, u'rdf:value'), note.xml_select(u'string(.)'))
-          )
-        ) for note in term.xml_select(u'termNote') ),
-        (E(RELATION_LOOKUP.get(rel.xml_select(u'string(relationType)'), (ZTHES_NAMESPACE, u'z:'+rel.xml_local)),
-          {(RDF_NAMESPACE, u'rdf:resource'): rel.xml_select(u'concat("#", termId)')}
-        ) for rel in term.xml_select(u'relation') )
-      )
-      #E((SKOS_NAMESPACE, u'skos:note'), term.xml_select(u'string(termName)')),
-    for term in doc.xml_select(u'/Zthes/term'))
-  )
-))
-
-print
-
