3323cf264a5cfafeb1daec21466678c99942adf8
Uche Ogbuji <uche@ogbuji.net>
2009-01-03 11:11:45 -0700

    Rearrange tests as suggested by Luis
    --HG--
    rename : test/test_quick_reference.py => test/docs/test_quick_reference.py
    rename : test/test_whatsnew.py => test/docs/test_whatsnew.py
    rename : test/whatsnew_doc1.xml => test/docs/whatsnew_doc1.xml

--- test/testall.sh
+++ test/testall.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+EXCLUSIONS="\
+#./bindery/test_basics.py \
+#./tree/test_gc.py \
+"
+
+echo $EXCLUSIONS
+
 for test in `find . -name "test_*.py"`; do
-  echo $test;
-  python $test $* ;
+    echo $EXCLUSIONS | grep $test > /dev/null 2>&1
+    if [ "$?" -eq "0" ]; then
+        echo "Skipped " $test;
+    else
+        echo $test;
+        python $test $* ;
+    fi
 done
 #Can't figure out a way to add echo to this version.  The -i supports the {}
 #ls -1 *.py | xargs -i python {}
