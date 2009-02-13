b55c08b7a219c2ee07dfd176524f0cfc8e4ca099
Jeremy Kloth <jeremy.kloth@4suite.org>
2009-02-13 10:11:48 -0700

    Fixed return type for AVT expressions

--- amara/xslt/expressions/avt.py
+++ amara/xslt/expressions/avt.py
@@ -4,6 +4,7 @@
 Implementation of XSLT attribute value templates
 """
 
+from amara.xpath import datatypes
 from amara.xpath.expressions import expression
 from amara.xslt import XsltError
 from amara.xslt.expressions import _avt
@@ -29,58 +30,16 @@ class avt_expression(expression):
         self._format = u''.join(parts)
         if not self._args:
             # use empty format args to force '%%' replacement
-            self._format %= ()
+            self._format = datatypes.string(self._format % ())
         return
 
     def evaluate_as_string(self, context):
         if not self._args:
             return self._format
-        return self._format % tuple(arg.evaluate_as_string(context)
-                                    for arg in self._args)
+        result = self._format % tuple(arg.evaluate_as_string(context)
+                                      for arg in self._args)
+        return datatypes.string(result)
     evaluate = evaluate_as_string
 
     def __str__(self):
         return '{' + self._format % tuple(self._args) + '}'
-
-
-class attribute_value_template:
-
-    def __init__(self, source, validator=None, element=None):
-        self.source = source
-        self.validator = validator
-        self.element = element
-
-        try:
-            # parts is a list of unicode and/or parsed XPath
-            parts = _AvtParser.parse(source)
-        except SyntaxError, exc:
-            raise XsltError(XsltError.AVT_SYNTAX)
-
-        self._resultFormat = u""
-        self._parsedParts = parsed_parts = []
-        for part in parts:
-            if isinstance(part, unicode):
-                if '%' in part:
-                    part = part.replace('%', '%%')
-                self._resultFormat += part
-            else:
-                self._resultFormat += u"%s"
-                parsed_parts.append(part)
-        return
-
-    def isConstant(self):
-        return not self._parsedParts
-
-    def evaluate(self, context):
-        if not self.element and hasattr(context, 'currentInstruction'):
-            self.element = context.currentInstruction
-
-        result = self._resultFormat % tuple([ convert(x.evaluate(context))
-                                              for x in self._parsedParts ])
-        if self.validator:
-            return self.validator.reprocess(self.element, result)
-        else:
-            return result
-
-    def __nonzero__(self):
-        return self.source is not None
