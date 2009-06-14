fe343a41119e3d2fcd0acb4e052ac528a6da0356
dalke <none@none>
2009-06-14 23:20:01 +0200

    Changed testall.sh to use the nosetests driver

--- test/testall.sh
+++ test/testall.sh
@@ -1,20 +1,9 @@
 #!/bin/sh
-#./bindery/test_basics.py \
-#./tree/test_gc.py \
-EXCLUSIONS="\
-#./bindery/test_performance.py \
-"
 
-echo $EXCLUSIONS
+# -v increases verbosity; shows the test names as they are being run
+# --exe to include test_*.py programs which are executable
+# -P to not tweak the PYTHONPATH to include ..
+# Use "-e" to exclude tests, using a regular expression#
 
-for test in `find . -name "test_*.py"`; do
-    echo $EXCLUSIONS | grep $test > /dev/null 2>&1
-    if [ "$?" -eq "0" ]; then
-        echo "Skipped " $test;
-    else
-        echo $test;
-        python $test $* ;
-    fi
-done
-#Can't figure out a way to add echo to this version.  The -i supports the {}
-#ls -1 *.py | xargs -i python {}
+nosetests -v --exe -P
+
