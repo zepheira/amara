38fcfe99e3f394fca7baee7b8fc3d4a519ad63c1
dalke <none@none>
2009-06-17 00:52:46 +0200

    Updated to use nosetests.

--- test/xpath/test_predicates.py
+++ test/xpath/test_predicates.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
-from amara.lib import testsupport
-from amara.xpath import context, datatypes
-from amara.xpath.expressions.basics import number_literal, string_literal
+from amara.xpath import context
 from amara.xpath.expressions.booleans import and_expr, or_expr
-
-from amara.test.xpath import base_xpath
 
 from test_expressions import (
     # boolean literals
@@ -15,33 +11,24 @@ from test_expressions import (
     nodeset_literal, ROOT
     )
 
+from amara.xpath.locationpaths.predicates import predicate
 
-class test_predicate_expr(base_xpath):
-    module_name = 'amara.xpath.locationpaths.predicates'
-
-    @classmethod
-    def new_tst_method(cls, expected, factory, args, nodes):
-        ctx = context(nodes[0], 1, len(nodes))
-        def test_method(self):
-            predicate = factory(*args)
-            result = predicate.select(ctx, nodes)
-            self.assertEquals(expected, list(result))
-        return test_method
-
-
-class test_predicate(test_predicate_expr):
-    class_name = 'predicate'
-    test_cases = [
+def test_predicate():
+    ctx = context(ROOT, 1, 1)
+    for args, expected in (
         # FIXME: test literal optimization
         # FIXME: test `position() = Expr` optimization
         # FIXME: test `position() [>,>=] Expr` optimization
         # FIXME: test `Expr [<,<=] position()` optimization
         # FIXME: test numeric-type expression
         # test boolean-type expression
-        ([and_expr(TRUE, 'and', FALSE)], [], [ROOT]),
-        ([or_expr(TRUE, 'or', FALSE)], [ROOT], [ROOT]),
+        (and_expr(TRUE, 'and', FALSE), []),
+        (or_expr(TRUE, 'or', FALSE), [ROOT]),
         # FIXME: test object-type expression
-        ]
+        ):
+        result = predicate(args).select(ctx, [ROOT])
+        result = list(result)
+        assert result == expected, (result, expected)
 
 if __name__ == '__main__':
-    testsupport.test_main()
+    raise SystemExit("Use nosetests")
