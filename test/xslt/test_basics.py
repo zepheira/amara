6a0136e7f44a4b8d8f5d8fd8daf6f33a6479e888
dalke <none@none>
2009-07-11 01:23:42 +0200

    Added diff support for simple text. Added ability to pass in args to the processor.  (Needed to capture message stream for testing expected messages)

--- test/xslt/test_basics.py
+++ test/xslt/test_basics.py
@@ -7,8 +7,9 @@ from amara.lib import inputsource, treecompare
 from amara.xpath import util
 
 def _run(source_xml, transform_xml, expected, parameters,
-              compare_method, source_uri=None, transform_uri=None):
-    P = processor()
+         compare_method, source_uri=None, transform_uri=None,
+         processor_kwargs={}):
+    P = processor(**processor_kwargs)
     source = inputsource(source_xml, source_uri)
     transform = inputsource(transform_xml, transform_uri)
     P.append_transform(transform)
@@ -17,6 +18,8 @@ def _run(source_xml, transform_xml, expected, parameters,
     result = str(P.run(source, parameters=parameters))
     try:
         diff = compare_method(result, expected)
+        diff = list(diff)
+        assert not diff, (source_xml, transform_xml, result, expected, diff)
     except Exception, err:
         # I don't have a quick way to tell which string caused
         # the error, so let the person debugging figure it out.
@@ -25,19 +28,44 @@ def _run(source_xml, transform_xml, expected, parameters,
         print "=== EXPECTED ==="
         print expected
         raise
-    diff = list(diff)
-    assert not diff, (source_xml, transform_xml, result, expected, diff)
 
 def _run_html(source_xml, transform_xml, expected, parameters=None,
-              source_uri=None, transform_uri=None):
+              source_uri=None, transform_uri=None,
+              processor_kwargs={}):
     _run(source_xml, transform_xml, expected, parameters,
-         treecompare.html_diff, source_uri, transform_uri)
+         treecompare.html_diff, source_uri, transform_uri,
+         processor_kwargs)
 
 def _run_xml(source_xml, transform_xml, expected, parameters=None,
+              source_uri=None, transform_uri=None,
+             processor_kwargs={}):
+    _run(source_xml, transform_xml, expected, parameters,
+         treecompare.xml_diff, source_uri, transform_uri,
+         processor_kwargs)
+
+def _compare_text(s1, s2):
+    if s1 == s2:
+        return []
+    for i in range(min(len(s1), len(s2))):
+        if s1[i] != s2[i]:
+            prefix = s1[:i]
+            break
+    else:
+        prefix = s1
+        i += 1
+    lineno = prefix.count("\n")+1
+    line_start = prefix.rfind("\n")+1
+    s1_rest = s1[i:i+20]
+    s2_rest = s2[i:i+20]
+    return ["Difference at line %d col %d" % (lineno, i-line_start+1),
+            "prefix: %r" % (s1[line_start:i],),
+            "s1 continues: %r" % (s1_rest,),
+            "s2 continues: %r" % (s2_rest,),]
+
+def _run_text(source_xml, transform_xml, expected, parameters=None,
               source_uri=None, transform_uri=None):
     _run(source_xml, transform_xml, expected, parameters,
-         treecompare.xml_diff, source_uri, transform_uri)
-
+         _compare_text, source_uri, transform_uri)
 
 def test_basics_1():
     _run_html('xslt/addr_book1.xml', 'xslt/addr_book1.xsl', 
