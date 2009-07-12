00bff44a03898d2559c66b3cf2593cf1f524c551
dalke <none@none>
2009-07-12 15:17:19 +0200

    Moved code over to "xslt_support" and changed how directory location is done

--- test/xslt/test_basics.py
+++ test/xslt/test_basics.py
@@ -2,78 +2,14 @@
 # test/xslt/test_basics.py
 import cStringIO
 
-from amara.xslt.processor import processor
-from amara.lib import inputsource, treecompare
-from amara.xpath import util
+from xslt_support import _run_html, _run_xml
+
+import os
+module_name = os.path.dirname(__file__)
 
 def find_file(filename):
-    import os
-    for prefix in ("", "xslt/", "test/xslt/"):
-        if os.path.exists(prefix+filename):
-            return prefix+filename
-    return filename
+    return os.path.join(module_name, filename)
 
-def _run(source_xml, transform_xml, expected, parameters,
-         compare_method, source_uri=None, transform_uri=None,
-         processor_kwargs={}):
-    P = processor(**processor_kwargs)
-    source = inputsource(source_xml, source_uri)
-    transform = inputsource(transform_xml, transform_uri)
-    P.append_transform(transform)
-    if parameters is not None:
-        parameters = util.parameterize(parameters)
-    result = str(P.run(source, parameters=parameters))
-    try:
-        diff = compare_method(result, expected)
-        diff = list(diff)
-        assert not diff, (source_xml, transform_xml, result, expected, diff)
-    except Exception, err:
-        # I don't have a quick way to tell which string caused
-        # the error, so let the person debugging figure it out.
-        print "=== RESULT ==="
-        print result
-        print "=== EXPECTED ==="
-        print expected
-        print "=== DONE ==="
-        raise
-
-def _run_html(source_xml, transform_xml, expected, parameters=None,
-              source_uri=None, transform_uri=None,
-              processor_kwargs={}):
-    _run(source_xml, transform_xml, expected, parameters,
-         treecompare.html_diff, source_uri, transform_uri,
-         processor_kwargs)
-
-def _run_xml(source_xml, transform_xml, expected, parameters=None,
-              source_uri=None, transform_uri=None,
-             processor_kwargs={}):
-    _run(source_xml, transform_xml, expected, parameters,
-         treecompare.xml_diff, source_uri, transform_uri,
-         processor_kwargs)
-
-def _compare_text(s1, s2):
-    if s1 == s2:
-        return []
-    for i in range(min(len(s1), len(s2))):
-        if s1[i] != s2[i]:
-            prefix = s1[:i]
-            break
-    else:
-        prefix = s1
-        i += 1
-    lineno = prefix.count("\n")+1
-    line_start = prefix.rfind("\n")+1
-    s1_rest = s1[i:i+20]
-    s2_rest = s2[i:i+20]
-    return ["Difference at line %d col %d" % (lineno, i-line_start+1),
-            "prefix: %r" % (s1[line_start:i],),
-            "s1 continues: %r" % (s1_rest,),
-            "s2 continues: %r" % (s2_rest,),]
-
-def _run_text(source_xml, transform_xml, expected, parameters=None,
-              source_uri=None, transform_uri=None):
-    _run(source_xml, transform_xml, expected, parameters,
-         _compare_text, source_uri, transform_uri)
 
 def test_basics_1():
     _run_html(find_file('addr_book1.xml'), find_file('addr_book1.xsl'), 
