2f5787d7df06b66f65f8a0bc272437720359194b
dalke <none@none>
2009-07-18 22:26:02 +0200

    upgraded to the new test framework using nose

--- test/xslt/borrowed/og_20010507.py
+++ test/xslt/borrowed/og_20010507.py
@@ -3,18 +3,12 @@
 # Oliver Graf <ograf@oli-ver-ena.de>:
 # ft:output cdata-section-elements is not used
 
-import os, tempfile
-import cStringIO
-import unittest
-
-from amara.lib import treecompare
-from amara.test import test_main
-from amara.test.xslt import xslt_test, filesource, stringsource
-
-from Ft.Lib.Uri import OsPathToUri
+import tempfile
+from amara.lib import iri
+from amara.test.xslt.xslt_support import _run_text
 
 FILESTEM = tempfile.mktemp()
-FILESTEM_URI = OsPathToUri(FILESTEM)
+FILESTEM_URI = iri.os_path_to_uri(FILESTEM)
 
 file_expected_1 = """\
 <?xml version="1.0" encoding="ISO-8859-1"?>
@@ -26,8 +20,9 @@ file_expected_1 = """\
   <what>test</what>
 </datatree>"""
 
-class test_xslt_cdata_section_elements_og_20010507(xslt_test):
-    source = stringsource("""\
+def test_xslt_cdata_section_elements_og_20010507():
+    _run_text(
+        source_xml = """\
 <?xml version="1.0" encoding="ISO-8859-1"?>
 
 <test>
@@ -35,8 +30,8 @@ class test_xslt_cdata_section_elements_og_20010507(xslt_test):
     <num>1000</num>
     <str>test</str>
   </data>
-</test>""")
-    transform = stringsource("""\
+</test>""",
+        transform_xml = """\
 <?xml version="1.0" encoding="ISO-8859-1"?>
 <xsl:stylesheet
     xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0"
@@ -56,26 +51,8 @@ class test_xslt_cdata_section_elements_og_20010507(xslt_test):
   </exsl:document>
 </xsl:template>
 
-</xsl:stylesheet>"""%(FILESTEM_URI))
-    parameters = {}
-    expected = """\
-"""
-
-    # def test_transform(self):
-    #     if os.path.exists(FILESTEM):
-    #         os.unlink(FILESTEM)
-    #     import sys
-    #     from amara.xslt import transform
-    #     result = transform(self.source, self.transform, output=io)
-    # 
-    #     tester.compare(1, os.path.exists(FILESTEM))
-    #     fileData = open(FILESTEM,'r').read()
-    #     tester.compare(file_expected_1, fileData)
-    #     tester.testDone()
-    #     if os.path.exists(FILESTEM):
-    #         os.unlink(FILESTEM)
-    # 
-    #     return
-
+</xsl:stylesheet>"""%(FILESTEM_URI),
+        expected = "\n"
+        )
 if __name__ == '__main__':
     test_main()
