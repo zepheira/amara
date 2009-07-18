6ec7cbc75d7471e24723a5912f29db80680d315c
dalke <none@none>
2009-07-18 15:30:22 +0200

    Updated to use the new test framework. Tests still don't pass. Don't know why.

--- test/xslt/borrowed/tg_20010628.py
+++ test/xslt/borrowed/tg_20010628.py
@@ -1,7 +1,7 @@
 ########################################################################
 # test/xslt/tg_20010628.py
 # Thomas Guettler <guettli@thomas-guettler.de> wants to use XInclude in 
-# his stylesheets
+# his stylesheets
 
 import os
 import cStringIO
@@ -9,14 +9,18 @@ import unittest
 
 from amara.lib import treecompare
 from amara.test import test_main
-from amara.test.xslt import xslt_test, filesource, stringsource
+#from amara.test.xslt import xslt_test, filesource, stringsource
+from amara.test.xslt.xslt_support import _run_html
 
-from Ft.Lib import Uri
-BASE_URI = Uri.OsPathToUri(os.path.abspath(__file__), attemptAbsolute=True)
-INCLUDE_URI = Uri.Absolutize('resources/tg_20010628-include.xml', BASE_URI)
+#from Ft.Lib import Uri
+#BASE_URI = Uri.OsPathToUri(os.path.abspath(__file__), attemptAbsolute=True)
+#INCLUDE_URI = Uri.Absolutize('resources/tg_20010628-include.xml', BASE_URI)
+INCLUDE_URI = "file:" + os.path.join(os.path.dirname(__file__),
+                                     "resources", "tg_20010628-include.xml")
 
-class test_xslt_xinclude_tg_20010628(xslt_test):
-    source = stringsource("""\
+def test_xslt_xinclude_tg_20010628():
+    _run_html(
+        source_xml = """\
 <?xml version="1.0" encoding="iso-8859-1"?>
 <html xmlns:xinclude="http://www.w3.org/2001/XInclude">
 <head>
@@ -25,8 +29,8 @@ class test_xslt_xinclude_tg_20010628(xslt_test):
 <body>
 <xinclude:include href="%(uri)s"/>
 </body>
-</html>""" % {'uri' : INCLUDE_URI})
-    transform = stringsource("""\
+</html>""" % {'uri' : INCLUDE_URI},
+        transform_xml = """\
 <?xml version="1.0" encoding="iso-8859-1"?>
 <xsl:stylesheet version="1.0"
                 xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
@@ -49,9 +53,8 @@ class test_xslt_xinclude_tg_20010628(xslt_test):
 </xsl:template>
 
 </xsl:stylesheet>
-""" % {'uri' : INCLUDE_URI})
-    parameters = {}
-    expected = """\
+""" % {'uri' : INCLUDE_URI},
+        expected = """\
 <html xmlns:xinclude="http://www.w3.org/2001/XInclude">
   <head>
     <meta http-equiv='Content-Type' content='text/html; charset=iso-8859-1'>
@@ -65,10 +68,7 @@ class test_xslt_xinclude_tg_20010628(xslt_test):
  some text
 </p>
   </body>
-</html>""" % {'base' : INCLUDE_URI}
-
-
+</html>""" % {'base' : INCLUDE_URI},
+        )
 if __name__ == '__main__':
-    test_main()
-
-
+    raise SystemExit("Use nosetests")
