3a5d84e043a730315b406ccce8527f4155c3cd52
dalke <none@none>
2009-07-18 02:19:15 +0200

    Updated test to be easier to understand, based on nosetests

--- test/xslt/borrowed/jlc_20090204.py
+++ test/xslt/borrowed/jlc_20090204.py
@@ -2,12 +2,10 @@
 # test/xslt/borrowed/jlc_20090204.py
 # John L. Clark: http://lists.fourthought.com/pipermail/4suite-dev/2009-February/002272.html
 
-from amara.lib import treecompare
-from amara.test import test_main
-from amara.test.xslt import xslt_test, filesource, stringsource
+from amara.test.xslt.xslt_support import _run_xml
 
-class test_jlc_20090204(xslt_test):
-    source = transform = stringsource("""<xsl:stylesheet
+def test_jlc_20090204():
+    source = """<xsl:stylesheet
     xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
     xmlns:test="tag:jlc6@po.cwru.edu,2009-02-04:testNS"
     version="1.0">
@@ -19,12 +17,15 @@ class test_jlc_20090204(xslt_test):
       <xsl:if test="current()[test:foo = 'bar']">current</xsl:if>
     </xsl:copy>
   </xsl:template>
-</xsl:stylesheet>""")
-    #source = stringsource("<doc/>")
-    parameters = {}
+</xsl:stylesheet>"""
     expected = """<?xml version="1.0" encoding="UTF-8"?>
 <xsl:stylesheet xmlns:test="tag:jlc6@po.cwru.edu,2009-02-04:testNS"
 xmlns:xsl="http://www.w3.org/1999/XSL/Transform">current</xsl:stylesheet>"""
 
+    _run_xml(
+        source_xml = source,
+        transform_xml = source,
+        expected = expected)
+
 if __name__ == '__main__':
-    test_main()
+    raise SystemExit("Use nosetests")
