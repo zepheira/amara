c6fa1dc4fd64d16518e9e401258056c1ffb7de22
dalke <none@none>
2009-07-18 15:36:55 +0200

    Updated the test framework. Tests don't seem to pass though.

--- test/xslt/borrowed/uo_20010503.py
+++ test/xslt/borrowed/uo_20010503.py
@@ -7,13 +7,12 @@
 
 import os
 
-from amara.lib import treecompare, iri
-from amara.test import test_main
-from amara.test.xslt import xslt_test, stringsource
+from amara.lib import iri
+from amara.test.xslt.xslt_support import _run_xml
 
 BASE_URI = iri.os_path_to_uri(os.path.abspath(__file__), attemptAbsolute=True)
 
-common_transform = stringsource("""
+common_transform = """
 <xsl:stylesheet
   xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
   version="1.0"
@@ -26,40 +25,38 @@ common_transform = stringsource("""
   </xsl:template>
 
 </xsl:stylesheet>
-""")
+"""
 
-class test_xslt_uo_20010503_1(xslt_test):
-  transform = common_transform
-
-  source = stringsource("""<?xml version='1.0'?>
+def test_xslt_uo_20010503_1():
+  _run_xml(
+    source_xml = """<?xml version='1.0'?>
 <x xmlns:xi="http://www.w3.org/2001/XInclude">
 <xi:include href="include1.xi"/>
 </x>
-""")
-
-  expected = """<?xml version="1.0" encoding="UTF-8"?>
+""",
+    transform_xml = common_transform,
+    expected = """<?xml version="1.0" encoding="UTF-8"?>
 <x xmlns:xi="http://www.w3.org/2001/XInclude">
 <foo xml:base="%s"/>
-</x>""" % iri.absolutize("include1.xi", BASE_URI)
+</x>""" % iri.absolutize("include1.xi", BASE_URI),
+    )
 
-
-class test_xslt_uo_20010503_2(xslt_test):
-  transform = common_transform
-
-  source = stringsource("""<?xml version='1.0'?>
+def test_xslt_uo_20010503_2():
+  _run_xml(
+    source_xml = """<?xml version='1.0'?>
   <x xmlns:xi="http://www.w3.org/2001/XInclude">
   <xi:include href="include2.xi"/>
   </x>
-  """)
-
-  expected = """<?xml version="1.0" encoding="UTF-8"?>
+  """,
+    transform_xml = common_transform,
+    expected = """<?xml version="1.0" encoding="UTF-8"?>
 <x xmlns:xi="http://www.w3.org/2001/XInclude">
 <foo xml:base="%s">
   <foo xml:base="%s"/>
 </foo>
 </x>""" % (iri.absolutize("include2.xi", BASE_URI),
-           iri.absolutize("include1.xi", BASE_URI))
-
+           iri.absolutize("include1.xi", BASE_URI)),
+    )
 
 # Determine platform line seperator for textual inclusions
 f = open(os.path.join(os.path.dirname(__file__), 'include1.xi'), 'rb')
@@ -71,20 +68,19 @@ else:
     # Assume UNIX line-ending
     LINESEP = '\n'
 
-class test_xslt_uo_20010503_3(xslt_test):
-  transform = common_transform
-
-  source = stringsource("""<?xml version='1.0'?>
+def test_xslt_uo_20010503_3():
+  _run_xml(
+    source_xml = """<?xml version='1.0'?>
 <x xmlns:xi="http://www.w3.org/2001/XInclude">
 <xi:include href="include2.xi" parse='text'/>
 </x>
-""")
-
-  expected = """<?xml version="1.0" encoding="UTF-8"?>
+""",
+    transform_xml = common_transform,
+    expected = """<?xml version="1.0" encoding="UTF-8"?>
 <x xmlns:xi="http://www.w3.org/2001/XInclude">
 &lt;?xml version='1.0' encoding='utf-8'?&gt;%(linesep)s&lt;foo xmlns:xi="http://www.w3.org/2001/XInclude"&gt;%(linesep)s  &lt;xi:include href="include1.xml"/&gt;%(linesep)s&lt;/foo&gt;
-</x>""" % {'linesep' : LINESEP}
-
+</x>""" % {'linesep' : LINESEP},
+    )
 
 if __name__ == '__main__':
-    test_main()
+    raise SystemExit("Use nosetests")
