d6e9ad908facb965ee48c03617ea98720f443e84
dalke <none@none>
2009-07-18 16:22:59 +0200

    Got the tests to work again. Well, excepting formatting. I don't know enough to know if the expected results are correct

--- test/xslt/borrowed/dm_20010506.py
+++ test/xslt/borrowed/dm_20010506.py
@@ -6,82 +6,109 @@ import os
 import cStringIO
 import unittest
 
-from amara.lib import treecompare
-from amara.test import test_main
-from amara.test.xslt import xslt_test, filesource, stringsource, XsltError
+from amara.xslt import XsltError
+from amara.lib import iri
+from amara.test.xslt.xslt_support import _run_xml, _run_html
 
-raise Exception, "FAIL, incomplete port, specific errors not tested"
+#raise Exception, "FAIL, incomplete port, specific errors not tested"
 
-commonsource = stringsource("""<ignored/>""")
+commonsource = "<ignored/>"
 
-class test_xslt_import_with_variables_dm_20010506(xslt_test):
-    source = commonsource
-    transform = stringsource("""<?xml version="1.0"?>
+def test_xslt_import_with_variables_dm_20010506():
+    _run_html(
+        source_xml = commonsource,
+        transform_uri = iri.os_path_to_uri(__file__),
+        transform_xml = """<?xml version="1.0"?>
 <xsl:stylesheet
   xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
   version='1.0'>
 
-  <xsl:import href="dm_20010506.xslt"/>
+  <xsl:import href="dm_20010506-1.xslt"/>
 
   <xsl:variable name="section.autolabel" select="1" />
   <xsl:variable name="html.stylesheet">book.xsl</xsl:variable>
   <xsl:variable name="html.stylesheet.type">text/css</xsl:variable>
     
-</xsl:stylesheet>""")
-    parameters = {}
-    expected = """\
-<html>\n    START\n    <link type='text/css' rel='stylesheet' href='book.xsl'>\n    END\n    </html>"""
+</xsl:stylesheet>""",
+        expected = """\
+<html>
+    START
+    <link type='text/css' rel='stylesheet' href='book.xsl'>
+    END
+    </html>""",
+        )
 
-class test_xslt_import_with_params_dm_20010506(xslt_test):
-    source = commonsource
-    transform = stringsource("""<?xml version="1.0"?>
+def test_xslt_import_with_params_dm_20010506():
+    _run_html(
+        source_xml = commonsource,
+        transform_uri = iri.os_path_to_uri(__file__),
+        transform_xml = """<?xml version="1.0"?>
 <xsl:stylesheet
   xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
   version='1.0'>
 
-  <xsl:import href="dm_20010506.xslt"/>
+  <xsl:import href="dm_20010506-1.xslt"/>
 
   <xsl:param name="section.autolabel" select="1" />
   <xsl:param name="html.stylesheet">book.xsl</xsl:param>
   <xsl:param name="html.stylesheet.type">text/css</xsl:param>
 
-</xsl:stylesheet>""")
+</xsl:stylesheet>""",
+        expected = """\
+<html>
+    START
+    <link type='text/css' rel='stylesheet' href='book.xsl'>
+    END
+    </html>""",
+        )
 
-class test_xslt_include_with_variables_dm_20010506(xslt_error):
-    error_code = XsltError.DUPLICATE_TOP_LEVEL_VAR
-    source = commonsource
-    transform = stringsource("""<?xml version="1.0"?>
+def test_xslt_include_with_variables_dm_20010506():
+    try:
+        _run_html(
+            source_xml = commonsource,
+            transform_uri = iri.os_path_to_uri(__file__),
+            transform_xml = """<?xml version="1.0"?>
 <xsl:stylesheet
   xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
   version='1.0'>
 
-  <xsl:include href="dm_20010506.xslt"/>
+  <xsl:include href="dm_20010506-1.xslt"/>
 
   <xsl:variable name="section.autolabel" select="1" />
   <xsl:variable name="html.stylesheet">book.xsl</xsl:variable>
   <xsl:variable name="html.stylesheet.type">text/css</xsl:variable>
 
-</xsl:stylesheet>""")
-    parameters = {}
-    expected = ""
+</xsl:stylesheet>""",
+            expected = None)
+    except XsltError, err:
+        assert err.code == XsltError.DUPLICATE_TOP_LEVEL_VAR, err
+    else:
+        raise AssertionError("Should have raised an exception")
 
-class test_xslt_include_with_params_dm_20010506(xslt_error):
-    error_code = XsltError.DUPLICATE_TOP_LEVEL_VAR
-    source = commonsource
-    transform = stringsource("""<?xml version="1.0"?>
+def test_xslt_include_with_params_dm_20010506():
+    try:
+        _run_html(
+            source_xml = commonsource,
+            transform_uri = iri.os_path_to_uri(__file__),
+            transform_xml = """<?xml version="1.0"?>
 <xsl:stylesheet
   xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
   version='1.0'>
 
-  <xsl:include href="dm_20010506.xslt"/>
+  <xsl:include href="dm_20010506-1.xslt"/>
 
   <xsl:param name="section.autolabel" select="1" />
   <xsl:param name="html.stylesheet">book.xsl</xsl:param>
   <xsl:param name="html.stylesheet.type">text/css</xsl:param>
 
-</xsl:stylesheet>""")
-    parameters = {}
-    expected= ""
+</xsl:stylesheet>""",
+            expected = None,
+            )
+    except XsltError, err:
+        assert err.code == XsltError.DUPLICATE_TOP_LEVEL_VAR, err
+    else:
+        raise AssertionError("Should have raised an exception")
+
 
 if __name__ == '__main__':
     test_main()
